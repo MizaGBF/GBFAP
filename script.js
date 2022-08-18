2ce834f97055a3978c9c7cfeb705afa2834f7bc3
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-08-18 18:29:31 +0200

    rollback, will look into it later

--- script.js
+++ script.js
@@ -55,14 +55,9 @@ define(["model/cjs-loader", "model/manifest-loader", "view/cjs_npc_demo"], funct
                     }
                     else{
                         l = b.getActionList();
-                        tl = [];
-                        for (action in l) {
-                            tl[action] = b.translateAction(l[action]);
-                        }
-                        tl.sort();
                         var actionlist = '<option value="default">Demo</option>'
-                        for (action in tl) {
-                            actionlist = actionlist.concat('<option value=' + l[action] + '>' + tl[action] + '</option>');
+                        for (action in l) {
+                            actionlist = actionlist.concat('<option value=' + l[action] + '>' + b.translateAction(l[action]) + '</option>');
                         }
                         document.getElementById("act-selection").innerHTML = actionlist
                     }
