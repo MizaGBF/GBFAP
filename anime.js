09d3ef91bc164ba4df7106bcf3f07595467a1b85
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-08-30 16:39:20 +0200

    updated gbfal link

--- anime.js
+++ anime.js
@@ -54,7 +54,7 @@ function successJSON(id)
     result_area = document.getElementById('result');
     
     var ref = document.createElement('a');
-    ref.setAttribute('href', "https://mizagbf.github.io/GBFAL/search.html?id=" + id);
+    ref.setAttribute('href', "https://mizagbf.github.io/GBFAL/search.html?id=" + id.split("_")[0]);
     ref.appendChild(document.createTextNode("Assets"));
     result_area.insertBefore(ref, result_area.firstChild);
     
