e45d4f030d8c1d7397f001161b7ef7d1ad57c3a6
MizaGBF <backupgbf1@gmail.com>
2023-02-16 15:20:34 +0100

    comments

--- anime.js
+++ anime.js
@@ -1,14 +1,15 @@
-var AnimeData = null;
-var AnimeLocal = false; // to use locally
+// main file
+var AnimeData = null; // will contain the Character data for the player
+var AnimeLocal = false; // set to true if assets are on the same machine
 var AnimeDebug = false; // debug only, ignore it
-var corsProxy = 'https://gbfcp2.onrender.com/'
+var corsProxy = 'https://gbfcp2.onrender.com/' // CORS Proxy (if AnimeLocal is true)
 var Game = {
     xjsUri: 'https://prd-game-a3-granbluefantasy.akamaized.net/assets_en/js',
     jsUri: corsProxy + 'https://prd-game-a3-granbluefantasy.akamaized.net/assets_en/js',
     imgUri: corsProxy + 'https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img',
     setting: {}
 };
-if(AnimeLocal)
+if(AnimeLocal) // local install version
 {
     Game = {
         xjsUri: '',
@@ -18,8 +19,8 @@ if(AnimeLocal)
     };
 }
 
-char_index = null;
-endpoints = [
+char_index = null; // character index (for the list of characters)
+endpoints = [ // possible asset endpoints
     "prd-game-a-granbluefantasy.akamaized.net/",
     "prd-game-a1-granbluefantasy.akamaized.net/",
     "prd-game-a2-granbluefantasy.akamaized.net/",
@@ -27,8 +28,11 @@ endpoints = [
     "prd-game-a4-granbluefantasy.akamaized.net/",
     "prd-game-a5-granbluefantasy.akamaized.net/"
 ];
-counter = 0;
+counter = 0; // endpoint counter (gbf doesn't support http/2 so we cycle through the endpoints)
 
+// generic xhr request function
+// id is passed to the callbacks
+// default timeout is 60s
 function get(url, callback, err_callback, id) {
     var xhr = new XMLHttpRequest();
     xhr.ontimeout = function () {
@@ -49,15 +53,19 @@ function get(url, callback, err_callback, id) {
     xhr.send(null);
 }
 
+// on success requesting a character json
 function successJSON(id)
 {
-    AnimeData = JSON.parse(this.response);
-    get(Game.jsUri + "/model/manifest/enemy_6204152.js", successLoading, failLoading, id);
+    AnimeData = JSON.parse(this.response); // parse the data
+    get(Game.jsUri + "/model/manifest/enemy_6204152.js", successLoading, failLoading, id); // load the player
+    // note: my CORS proxy is hosted on a free render.com tier
+    // this step add a loading animation while the proxy wakes up
 }
 
+// on error requesting a character json
 function failJSON(id)
 {
-    if(!AnimeDebug)
+    if(!AnimeDebug) // call debug mode (can be disabled by setting AnimeDebug to true)
     {
         let d = getDebug();
         if(!AnimeLocal && d != null) // testing only
@@ -69,36 +77,30 @@ function failJSON(id)
                 imgUri: corsProxy + d + '/debug/https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img',
                 setting: {}
             }
-            get(corsProxy + d + "/json/" + id + ".json?" + Date.now(), successJSON, failDebug, id);
+            get(corsProxy + d + "/json/" + id + ".json?" + Date.now(), successJSON, failJSON, id);
             return;
         }
     }
+    // remove loading image and put error message
     document.getElementById('temp').remove();
     result_area = document.getElementById('result');
     removeChilds(result_area);
     result_area.appendChild(document.createTextNode("Error: Invalid ID"));
 }
 
-function failDebug(id)
-{
-    document.getElementById('temp').remove();
-    result_area = document.getElementById('result');
-    removeChilds(result_area);
-    result_area.appendChild(document.createTextNode("Error: Invalid ID"));
-}
-
+// on success loading the player
 function successLoading(id)
 {
-    document.getElementById('temp').remove();
+    document.getElementById('temp').remove(); // remove loading image
     result_area = document.getElementById('result');
-    let ref = document.createElement('a');
+    let ref = document.createElement('a'); // add GBFAL link
     ref.setAttribute('href', "https://mizagbf.github.io/GBFAL/?id=" + id.split("_")[0]);
     ref.appendChild(document.createTextNode("Assets"));
     result_area.insertBefore(ref, result_area.firstChild);
     
     result_area.insertBefore(document.createElement('br'), result_area.firstChild);
     
-    ref = document.createElement('a');
+    ref = document.createElement('a'); // add wiki link
     ref.setAttribute('href', "https://gbf.wiki/index.php?title=Special:Search&search=" + id);
     ref.appendChild(document.createTextNode("Wiki"));
     result_area.insertBefore(ref, result_area.firstChild);
@@ -107,10 +109,10 @@ function successLoading(id)
     
     if(!AnimeDebug)
     {
-        let img = document.createElement("img");
+        let img = document.createElement("img"); // add character thumbnail on top
         result_area.insertBefore(img, result_area.firstChild);
         img.id  = "loading";
-        img.onerror = function() {
+        img.onerror = function() { // can't be loaded? character doesn't exist
             let result = this.parentNode.parentNode;
             this.parentNode.remove();
             this.remove();
@@ -126,12 +128,14 @@ function successLoading(id)
             img.src = "https://prd-game-a1-granbluefantasy.akamaized.net/assets_en/img_low/sp/assets/npc/m/" + el[0] + "_01_" + el[1] + ".jpg";
     }
 
+    // load the player
     require(["createjs"], function (b) {
         window.createjs = b
     })
     require(['player','lib/common', 'view/cjs', 'script', 'jquery', 'underscore', 'model/cjs-loader'])
 }
 
+// on error loading the player
 function failLoading(id)
 {
     document.getElementById('temp').remove();
@@ -140,6 +144,7 @@ function failLoading(id)
     result_area.appendChild(document.createTextNode("Error: Couldn't load the ID, please reload the page"));
 }
 
+// function to remove child elements
 function removeChilds(element)
 {
     let child = element.lastElementChild; 
@@ -149,29 +154,27 @@ function removeChilds(element)
     }
 }
 
+// function to get current page parameters
 function getParam()
 {
     let params = new URLSearchParams(window.location.search);
     return params.get("id");
 }
 
+// debug only, ignore
 function getDebug()
 {
     let params = new URLSearchParams(window.location.search);
     return params.get("debug");
 }
 
+// entry point, called on page loading
 function playAnimation()
 {
     if(getParam() == null)
     {
         document.getElementById('result').remove();
     }
-    startupCallback();
-}
-
-function startupCallback()
-{
     let id = getParam();
     if(id != null)
     {
@@ -189,6 +192,7 @@ function startupCallback()
     }
 }
 
+// return an asset endpoint
 function getEndpoint()
 {
     let e = endpoints[counter];
@@ -196,6 +200,7 @@ function getEndpoint()
     return e;
 }
 
+// add image to node
 function addImage(node, path, id, d = null)
 {
     let img = document.createElement("img");
@@ -214,6 +219,7 @@ function addImage(node, path, id, d = null)
     img.src = "https://" + getEndpoint() + "assets_en/img_low/" + path; 
 }
 
+// on success loading the index, will add image and link for every indexed characters
 function successDisplay(key)
 {
     if(!char_index) char_index = JSON.parse(this.response);
@@ -234,12 +240,14 @@ function successDisplay(key)
     }
 }
 
+// on error loading the index
 function failDisplay(id)
 {
     let node = document.getElementById('areacharacters');  
     result_area.appendChild(document.createTextNode("Failed to load character list"));
 }
 
+// function to load and build the index
 function displayCharacters(elem, key)
 {
     if(!char_index) get("json/index.json?" + Date.now(), successDisplay, failDisplay, key);
