aaa267f21ec221cf98f3f30db07b6ba650e2203a
MizaGBF <backupgbf1@gmail.com>
2023-11-20 11:34:42 +0100

    4.6 hidden enemy selector

--- view/cjs_npc_demo.js
+++ view/cjs_npc_demo.js
@@ -1,7 +1,13 @@
 define(["view/cjs", "view/content", "lib/common"], function (a, b) {
     var c = function(){
-        let arr = ["enemy_9100211", "enemy_9100221", "enemy_6204152"];
-        return arr[Math.floor(Math.random() * arr.length)];
+        let enemy = new URLSearchParams(window.location.search).get("enemy");
+        if(enemy == null)
+        {
+            let arr = ["enemy_9100211", "enemy_9100221", "enemy_6204152"];
+            return arr[Math.floor(Math.random() * arr.length)];
+        }
+        else
+            return "enemy_"+enemy;
     }();
     var canvasSize = 2000;
     var realSize = 1400;
