091d616850d313f2df4c4bb3d8bb362fd03e622c
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-05-23 14:42:41 +0200

    Cleaned audio, added catch to play()

--- js/audio.js
+++ js/audio.js
@@ -12,7 +12,9 @@ define([], (function() {
 			play: function(file)
 			{
 				if(this.disabled)
+				{
 					return;
+				}
 				try
 				{
 					let audio = null;
@@ -22,7 +24,9 @@ define([], (function() {
 						// if already playing
 						let i = this.instances.indexOf(audio);
 						if(i != -1)
+						{
 							this.instances.splice(i, 1); // remove from playing
+						}
 						audio.currentTime = 0; // reset head position
 					}
 					else
@@ -36,8 +40,18 @@ define([], (function() {
 					audio.volume = this.volume; // set volume to master
 					audio.muted = !player.is_audio_enabled(); // set muted attribute
 					audio.playbackRate = player.get_speed(); // set speed (seems to work between 0.01 ~ 4.0)
-					audio.play(); // play the audio
-					this.instances.push(audio); // add to playing
+					// play the audio
+					audio.play()
+					.then(() => {
+						this.instances.push(audio);
+					})
+					.catch((error) => {
+						// nothing, this is just to mute the following errors:
+						// Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first.
+					});
+					
+					
+					//this.instances.push(audio); // add to playing
 				} catch(err) {
 					console.error("Error attempting to play " + file, err);
 				}
@@ -63,12 +77,7 @@ define([], (function() {
 			set_master_volume: function(value)
 			{
 				// check bounds and set volume
-				if(value > 1.0)
-					this.volume = 1.0;
-				else if(value < 0.0)
-					this.volume = 0.0;
-				else
-					this.volume = value;
+				this.volume = Math.max(0.0, Math.min(1.0, value));
 				// apply to playing instances
 				for(let i = 0; i < this.instances.length; ++i)
 				{
@@ -79,33 +88,45 @@ define([], (function() {
 			{
 				let i = window.audio.instances.indexOf(this);
 				if(i != -1)
+				{
 					window.audio.instances.splice(i, 1);
+				}
 			},
 			stop_all: function() // "stop" and clear all playing audios
 			{
 				for(let i = 0; i < this.instances.length; ++i)
+				{
 					this.instances[i].pause();
+				}
 				this.instances = [];
 			},
 			pause_all: function() // pause all
 			{
 				for(let i = 0; i < this.instances.length; ++i)
+				{
 					this.instances[i].pause();
+				}
 			},
 			resume_all: function() // resume all
 			{
 				for(let i = 0; i < this.instances.length; ++i)
+				{
 					this.instances[i].play();
+				}
 			},
 			set_playback_speed: function() // set playback speed
 			{
 				for(let i = 0; i < this.instances.length; ++i)
+				{
 					this.instances[i].playbackRate = player.get_speed();
+				}
 			},
 			update_mute: function() // update unmute status
 			{
 				for(let i = 0; i < this.instances.length; ++i)
+				{
 					this.instances[i].muted = !player.is_audio_enabled();
+				}
 			}
 		};
 	}
